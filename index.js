5f1bef8bb4a45532a67a22b140ea5df78d020704
Adam Eleraky <adam_eleraky@hotmail.com>
2024-08-24 11:27:21 +0200

    href links added to some links

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let typed = new Typed('.multiple-text', {
-    strings: ["Fullstack Developer", "Backend Developer", "Frontend Developer"], 
+    strings: ["Fullstack", "Backend", "Frontend"], 
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 500,
