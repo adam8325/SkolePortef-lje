88827d510c7705b20bbdd273412c2cb0a0939831
Adam Eleraky <adam_eleraky@hotmail.com>
2024-08-24 10:17:37 +0200

    New layout setup

---
+++ index.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.multiple-text', {
+    strings: ["Fullstack Developer", "Backend Developer", "Frontend Developer"], 
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+});
