8967b242e76cf0c86bbf5ac34533002913197d5c
Adam Eleraky <adam_eleraky@hotmail.com>
2024-08-25 20:05:31 +0200

    js event added to change color of navlink when clicked

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const navbarLinks = document.querySelectorAll(".navbar-link");
+
+navbarLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    navbarLinks.forEach(navLink => navLink.style.color = "");
+    link.style.color = "#0ef";
+  });
+});
+
+
+
+
+
+
 let typed = new Typed('.multiple-text', {
     strings: ["Fullstack", "Backend", "Frontend"], 
     typeSpeed: 100,
